68ef64a09a8abbf3fbf7d2a724fa6f8feef7cac7
Vlad <rotariuvladimir@gmail.com>
2023-07-22 23:06:28 +0200

    Create game grids flexible dimension, methods initial setup living cells

---
+++ app/GameOfLife.js
@@ -0,0 +1,34 @@
+class GameOfLife {
+  dimensionSquareGrid;
+
+  constructor(dimensionSquareGrid) {
+    this.dimensionSquareGrid = dimensionSquareGrid;
+  }
+
+  createDeadGrid() {
+    const grid = [];
+
+    for (let row = 0; row < this.dimensionSquareGrid; row++) {
+      grid.push([]);
+    }
+
+    for (let row = 0; row < this.dimensionSquareGrid; row++) {
+      for (let cell = 0; cell < this.dimensionSquareGrid; cell++) {
+        grid[row][cell] = "🍂";
+      }
+    }
+
+    this.currentState = grid;
+  }
+
+  populateDeadGrid(initialPopulationRate) {
+    for (let row = 0; row < this.dimensionSquareGrid; row++) {
+      for (let cell = 0; cell < this.dimensionSquareGrid; cell++) {
+        if (Math.random() <= initialPopulationRate)
+          this.currentState[row][cell] = "🍃";
+      }
+    }
+  }
+}
+
+export default GameOfLife;
