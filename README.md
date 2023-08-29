6abe8187fa8775cef9f2699961df4646dac1b49c
Vlad <rotariuvladimir@gmail.com>
2023-08-29 20:42:24 +0200

    Modify readme and explain further code with comments

--- README.md
+++ README.md
@@ -1,13 +1,66 @@
-# Boilerplate for React apps
+# Conway's  Game of Life
 
-With Vite + TypeScript
+## Description
+
+We present an OOP implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life). Although one might find more concise implementations of Conway's Game of Life, we designed ours respecting Unix philosophy principles: see the [Implementation](#implementation) section.
+
+## Play
+
+Conway's Game of Life is a notorious [zero-player game](https://en.wikipedia.org/wiki/Zero-player_game), so all that you have to do to play is to:
+* [CLICK HERE!](https://conways-game-of-life-reimplemented.netlify.app/)
+
+## Tech stack:
+* Web: TypeScript, HTML5, CSS3.
+* OOP principles
+
+## Implementation
+The parameters of a 'game' instance of the ```GameOfLife``` class:
+```
+// Initialize the game parameters
+
+// The grid size is 30x30
+const dimensionSquareGrid = 30;
+
+// 15% of the grid will be populated initially
+const initialPopulationRate = 0.15; 
+
+// The game will run for 10000 generations
+const numberGenerations = 10 ** 4;
+
+// Time in milliseconds between each generation
+const speedTransition = 600; 
+
+// Using a blue circle emoji to represent alive cells
+const lifePattern = "🔵"; 
+
+// Using a black circle emoji to represent dead cells
+const deathPattern = "⚫"; 
+```
+Only the high-level parameters (i.e. grid dimension and styles) are properties of the class:
+```
+constructor(
+    private readonly dimensionSquareGrid: number, 
+    private readonly lifePattern: string, 
+    private readonly deathPattern: string, 
+  ) {}
+```
+The methods are simple in nature and do a single thing, obeying the UNIX philosophy/KISS principle:
+```
+#countDeadNeighbours(neighbours: string[]): number {
+    let deathCount = 0;
+    neighbours.forEach((neighbour) => {
+      if (neighbour === this.deathPattern) deathCount++;
+    });
+    return deathCount;
+  }
+```
 
 ## Scripts
 
-`npm run dev`: starts a development server
+`npm run dev`: starts a development server for the Game of Life
 
-`npm run build`: builds the app
+`npm run build`: builds the Game of Life app
 
-`npm run preview`: runs the built app
+`npm run preview`: runs the Game of Life built app
 
 `npm run lint`: runs ESLint
--- src/ts/GameOfLifeClass/GameOfLife.ts
+++ src/ts/GameOfLifeClass/GameOfLife.ts
@@ -1,21 +1,24 @@
 class GameOfLife {
-  private currentGridState = [[""]];
+  private currentGridState = [[""]]; // Holds the current state of the grid
 
   constructor(
-    private readonly dimensionSquareGrid: number,
-    private readonly lifePattern: string,
-    private readonly deathPattern: string,
+    private readonly dimensionSquareGrid: number, // Dimensions of the grid
+    private readonly lifePattern: string, // Representation of a living cell
+    private readonly deathPattern: string, // Representation of a dead cell
   ) {}
 
+  // Initializes a dead grid
   createDeadGrid(): void {
     const grid = [[""]];
 
+    // Initialize the grid
     for (let row = 0; row < this.dimensionSquareGrid; row++) {
       grid.push([]);
     }
 
     grid.shift();
 
+    // Populate grid with 'dead' cells
     for (let row = 0; row < this.dimensionSquareGrid; row++) {
       for (let cell = 0; cell < this.dimensionSquareGrid; cell++) {
         grid[row].push(this.lifePattern);
@@ -25,6 +28,7 @@ class GameOfLife {
     this.currentGridState = grid;
   }
 
+  // Populates the grid based on an initial population rate
   populateDeadGrid(initialPopulationRate: number): void {
     for (let row = 0; row < this.dimensionSquareGrid; row++) {
       for (let cell = 0; cell < this.dimensionSquareGrid; cell++) {
@@ -34,6 +38,7 @@ class GameOfLife {
     }
   }
 
+  // Returns the current state of the grid as a string
   displayState(): string {
     let statePositioned = "";
     for (let row = 0; row < this.dimensionSquareGrid; row++) {
@@ -43,6 +48,7 @@ class GameOfLife {
     return statePositioned;
   }
 
+  // Iterates through the grid and updates each cell's state
   changeStateGrid(): void {
     const oldGridState = this.currentGridState;
     for (let row = 0; row < this.dimensionSquareGrid; row++) {
@@ -52,6 +58,7 @@ class GameOfLife {
     }
   }
 
+  // Internal: Updates the state of a corner cell
   #changeStateCell(row: number, cell: number, oldGridState: string[][]): void {
     const neighbours = [];
     switch (true) {
@@ -152,13 +159,14 @@ class GameOfLife {
     }
   }
 
+  // Internal: Updates the state of a corner cell
   #changeStateCornerCell(
     neighbours: string[],
     oldGridState: string[][],
     row: number,
     cell: number,
   ): void {
-    const deathCount = this.#countDeathNeighbours(neighbours);
+    const deathCount = this.#countDeadNeighbours(neighbours);
 
     if (oldGridState[row][cell] === this.deathPattern && deathCount === 0) {
       this.currentGridState[row][cell] = this.lifePattern;
@@ -179,7 +187,7 @@ class GameOfLife {
     row: number,
     cell: number,
   ): void {
-    const deathCount = this.#countDeathNeighbours(neighbours);
+    const deathCount = this.#countDeadNeighbours(neighbours);
 
     if (oldGridState[row][cell] === this.deathPattern && deathCount === 2) {
       this.currentGridState[row][cell] = this.lifePattern;
@@ -199,13 +207,14 @@ class GameOfLife {
     this.currentGridState[row][cell] = oldGridState[row][cell];
   }
 
+  // Internal: Updates the state of a middle cell
   #changeStateMiddleCell(
     neighbours: string[],
     oldGridState: string[][],
     row: number,
     cell: number,
   ): void {
-    const deathCount = this.#countDeathNeighbours(neighbours);
+    const deathCount = this.#countDeadNeighbours(neighbours);
 
     if (oldGridState[row][cell] === this.deathPattern && deathCount === 5) {
       this.currentGridState[row][cell] = this.lifePattern;
@@ -225,7 +234,8 @@ class GameOfLife {
     this.currentGridState[row][cell] = oldGridState[row][cell];
   }
 
-  #countDeathNeighbours(neighbours: string[]): number {
+  // Internal: Counts the number of dead neighbors for a given cell
+  #countDeadNeighbours(neighbours: string[]): number {
     let deathCount = 0;
     neighbours.forEach((neighbour) => {
       if (neighbour === this.deathPattern) deathCount++;
